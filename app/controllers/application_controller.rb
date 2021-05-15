e832dc87571915c882cfb397cd21ba8468d8594c
Scott Serok <scott@serok.us>
2021-05-15 21:15:01 +0900

    adds around_actions set_locale and set_timezone
    fixes ensure_profile! for unauthenticated actions like sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :ensure_profile!
+  around_action :set_locale
+  around_action :set_timezone
 
   protected
 
   def ensure_profile!
+    return unless current_user
     return if current_user.profile
     redirect_to new_profile_path, notice: t('notice.complete_registration')
+  end
+
+  def set_locale(&block)
+    locale = (params[:locale] || current_user&.profile&.locale || 'en')
+    I18n.with_locale(locale, &block)
+  rescue I18n::InvalidLocale
+  end
+
+  def set_timezone(&block)
+    Time.use_zone((current_user&.profile&.timezone || 'UTC'), &block)
   end
 end
