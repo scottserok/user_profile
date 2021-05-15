5c727822a9b038e36776d7f91a14b281cf207e62
Scott Serok <scott@serok.us>
2021-05-15 22:25:51 +0900

    updates Profile timezone enum definition sorted

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   enum locale: I18n.available_locales.map { |x| [x.to_s, x.to_s] }.to_h
-  enum timezone: ActiveSupport::TimeZone.all.map { |x| [x.tzinfo.name, x.tzinfo.name] }.to_h
+  enum timezone: ActiveSupport::TimeZone.all.map { |x| [x.tzinfo.name, x.tzinfo.name] }.sort_by(&:first).to_h
 end
