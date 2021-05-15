f10bdb14bd037862c24dd4de1ac29fd71cc35284
Scott Serok <scott@serok.us>
2021-05-15 12:55:51 +0900

    updates Profile model timezone and locale enums
    Load the available locales and time zones when the application loads.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  enum locale: I18n.available_locales.map { |x| [x, x] }.to_h
+  enum timezone: ActiveSupport::TimeZone.all.map { |x| [x.tzinfo.name, x.tzinfo.name] }.to_h
 end
